1e77dca87a7e0308234bec0cb11e576514a83eb9
erwinislasegura <erwin.2785@gmail.com>
2026-04-18 00:46:54 -0400

    Fix bootstrap and URLs to serve landing page from project root

--- app/views/layouts/partials/sidebar.php
+++ app/views/layouts/partials/sidebar.php
@@ -1,13 +1,13 @@
 <h5 class="mb-3">Gestión de Cuotas</h5>
 <ul class="nav flex-column small">
-  <li class="nav-item"><a class="nav-link text-white" href="/panel">Panel</a></li>
-  <li class="nav-item"><a class="nav-link text-white" href="/socios">Socios</a></li>
-  <li class="nav-item"><a class="nav-link text-white" href="/cuotas">Cuotas</a></li>
-  <li class="nav-item"><a class="nav-link text-white" href="/pagos">Pagos</a></li>
-  <li class="nav-item"><a class="nav-link text-white" href="/aportes">Aportes</a></li>
-  <li class="nav-item"><a class="nav-link text-white" href="/egresos">Egresos</a></li>
-  <li class="nav-item"><a class="nav-link text-white" href="/tesoreria">Tesorería</a></li>
-  <li class="nav-item"><a class="nav-link text-white" href="/reportes">Reportes</a></li>
-  <li class="nav-item"><a class="nav-link text-white" href="/usuarios">Usuarios</a></li>
-  <li class="nav-item"><a class="nav-link text-white" href="/auditoria">Auditoría</a></li>
+  <li class="nav-item"><a class="nav-link text-white" href="<?= htmlspecialchars(url('panel')) ?>">Panel</a></li>
+  <li class="nav-item"><a class="nav-link text-white" href="<?= htmlspecialchars(url('socios')) ?>">Socios</a></li>
+  <li class="nav-item"><a class="nav-link text-white" href="<?= htmlspecialchars(url('cuotas')) ?>">Cuotas</a></li>
+  <li class="nav-item"><a class="nav-link text-white" href="<?= htmlspecialchars(url('pagos')) ?>">Pagos</a></li>
+  <li class="nav-item"><a class="nav-link text-white" href="<?= htmlspecialchars(url('aportes')) ?>">Aportes</a></li>
+  <li class="nav-item"><a class="nav-link text-white" href="<?= htmlspecialchars(url('egresos')) ?>">Egresos</a></li>
+  <li class="nav-item"><a class="nav-link text-white" href="<?= htmlspecialchars(url('tesoreria')) ?>">Tesorería</a></li>
+  <li class="nav-item"><a class="nav-link text-white" href="<?= htmlspecialchars(url('reportes')) ?>">Reportes</a></li>
+  <li class="nav-item"><a class="nav-link text-white" href="<?= htmlspecialchars(url('usuarios')) ?>">Usuarios</a></li>
+  <li class="nav-item"><a class="nav-link text-white" href="<?= htmlspecialchars(url('auditoria')) ?>">Auditoría</a></li>
 </ul>
